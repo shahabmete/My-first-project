8fe2ae1fa52516d2d7b46f80063e8a2e652df072
shahabmete <135550475+shahabmete@users.noreply.github.com>
2024-02-15 23:43:05 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let count = 0;
+const countSpan = document.getElementById('count');
+const incrementBtn = document.getElementById('incrementBtn');
+const decrementBtn = document.getElementById('decrementBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+function updateCount() {
+    countSpan.textContent = count;
+}
+
+incrementBtn.addEventListener('click', () => {
+    count++;
+    updateCount();
+});
+
+decrementBtn.addEventListener('click', () => {
+    count--;
+    updateCount();
+});
+
+resetBtn.addEventListener('click', () => {
+    count = 0;
+    updateCount();
+});
